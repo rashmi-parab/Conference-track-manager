ff1cb181f8ffaeeee88b44292455088743702a59
rashmi-parab <rashcp1991@gmail.com>
2018-04-30 11:41:49 +0200

    1. Rename ConferenceTrackManager to TrackManager 2. Refactor TrackManager to accept filepath as a command line argument

--- src/main/scala/TrackManager.scala
+++ src/main/scala/TrackManager.scala
@@ -1,14 +1,10 @@
 import helpers.InputFileParser
 
-import scala.io.StdIn.readLine
-
-object ConferenceTrackManager {
+object TrackManager {
 
   def main(args: Array[String]): Unit = {
 
-    val fileName = readLine("Enter the input file path : ")
-
-    val talkDetails = InputFileParser.parse(fileName)
+    val talkDetails = InputFileParser.parse(args.head)
 
     TrackBuilder.build(talkDetails) match {
       case Some(tracks) => TrackPrinter.print(tracks)
